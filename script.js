47962bfc1f98e2eabab288f8b82aeaf21c3fd0ec
Juan David Serpa Banquez <130329772+TheJuanChoCraft@users.noreply.github.com>
2025-05-28 17:08:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('formulario').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert("¡Gracias por registrarte! Nos vemos en el Festival de Postres 🎉");
+});
